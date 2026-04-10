d3789f0dccdd8908c3c8c601feefdab16365c157
Ahmedatef569 <ahmedatef569@gmail.com>
2026-04-10 16:10:02 +0200

    Add files via upload

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/dmc-dashboard/sw.js', { scope: '/dmc-dashboard/' })})}
